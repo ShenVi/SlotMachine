24c25466010e19ba7bd36523c97c4ab266080ed6
ShenVi <yvban@outlook.com>
2019-04-10 02:56:44 +0800

    游戏中奖判断以及奖励判断 生成账单返回账单信息

--- app/game/views.py
+++ app/game/views.py
@@ -5,17 +5,18 @@ from flask import render_template, request, session, redirect
 from flask_login import current_user
 from app.redis import establish_token
 from app.DB_account import Account
+from app.DB_module import Bill
 from app.payment import purchase, gamereward
 import random
 
 import json
 
 #获取游戏结果
-@game.route('/get-result')
+@game.route('/get-result', methods=["POST"])
 def result():
     if current_user.is_authenticated:
         if request.method == 'POST':
-            jsondata = request.json
+            #jsondata = request.json
 
             #token = jsondata['token']
 
@@ -32,9 +33,25 @@ def result():
                 b = random.choice([1,2,3,4,5,6,7,8,9,10,11,12])
                 c = random.choice([1,2,3,4,5,6,7,8,9,10,11,12])
 
-                reward = result_reward(a, b, c)
+                reward, rewardtype, roc = result_reward(a, b, c)
+                
+                jsons = {
+                    'code':1,
+                    'a':a,
+                    'b':b,
+                    'c':c,
+                    'reward':reward,
+                    'type':rewardtype,
+                    'money':Account.query.filter_by(id=current_user.id).first().money,
+                    'roc_date':roc.date,
+                    'roc_expendituremoney':roc.expendituremoney,
+                    'roc_income':roc.income,
+                    'roc_money':roc.money
+                }
 
-                return json.dumps({'code':1, 'a':a, 'b':b, 'c':c, 'gamereward':reward['addmoney']})
+                print(jsons)
+
+                return json.dumps(jsons)
 
             else:
                 return json.dumps({'code':0, 'text':'游戏币不足'})
@@ -45,16 +62,24 @@ def result():
         pass
 
 def result_reward(a, b, c):
-    if a == b & c:
+    if a == b & a == c:
         gamereward(3)
-        return {'type':3, 'addmoney':50}
+        roc = Bill(expendituremoney=None, income=50, money=Account.query.filter_by(id=current_user.id).first().money)
+        return 50, 3, roc
 
     elif a == b or b == c:
         gamereward(2)
-        return {'type':2, 'addmoney':5}
+        roc = Bill(expendituremoney=None, income=20, money=Account.query.filter_by(id=current_user.id).first().money)
+        return 20, 2, roc
+
+    elif a == c:
+        gamereward(2)
+        roc = Bill(expendituremoney=None, income=20, money=Account.query.filter_by(id=current_user.id).first().money)
+        return 20, 31, roc
 
     else:
-        return {'type':0, 'addmoney':0}
+        roc = Bill(expendituremoney=5, income=0, money=Account.query.filter_by(id=current_user.id).first().money)
+        return 0, 0, roc
 
 
 #验证密匙
